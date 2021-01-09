bd0e6f02711248f776788bc5faea9dd62783a8bc
Q-100 <opea5954@gmail.com>
2021-01-09 10:27:36 +0900

    java_study_210109

---
+++ src/JavaStudy_Class_210109.java
@@ -0,0 +1,24 @@
+public class JavaStudy_Class_210109 {
+    public static void main(String[] args) {
+        System.out.println(Mymath2.add_static(0.15,0.49)); // 클래스 메소드는 바로사용가능
+
+        Mymath2 t = new Mymath2(); // 인스턴스 메소드는 객체를 생성해야 사용가능
+        t.a = 0.15;
+        t.b = 0.49;
+        System.out.println(t.add());
+
+    }
+    static double add_static1(double a,double b){
+        return a+b;
+    }
+}
+class Mymath2{
+    double a,b;
+
+    double add(){ // 인스턴스 매소드
+        return this.a + this.b;
+    }
+    static double add_static(double a,double b){ // 클래스 메소드(인스턴스 변수를 사용하지 않을 때 사용)
+        return a+b;
+    }
+}
---
+++ src/JavaStudy_constructor_210109.java
@@ -0,0 +1,61 @@
+public class JavaStudy_constructor_210109 {
+    public static void main(String[] args) {
+        Data1 d1 = new Data1(5);
+        Data2 d2 = new Data2(5); // Data2 클래스에는 생성자가 존재하기 때문에 인자가 필요함
+        Car2 c1 = new Car2(); // Car2 매개변수 없는 생성자
+        Car2 c2 = new Car2("blue"); // 색깔만 넘겨주는 생성자
+        Car2 c3 = new Car2("green","auto",5); // 3개다 넘겨주는 생성자
+
+        System.out.println(c1.color + c1.gearType + c1.door);
+        System.out.println(c2.color + c2.gearType + c2.door);
+        System.out.println(c3.color + c3.gearType + c3.door);
+
+    }
+}
+class Data1{ 
+    int value;
+    Data1(){ // 매개변수가 없으면 3
+        value = 3;
+    }
+    Data1(int x){ // 매개변수가 있으면 매개변수를 value에 저장
+        value = x;
+    }
+}
+class Data2{
+    int value;
+    
+    Data2(int x){
+        value= x;
+    }
+}
+
+class Car2{
+    String color;
+    String gearType;
+    int door;
+    /*
+    Car2(String color){
+    door = 5;            // this를 사용할떈 무조건 첫번째 줄에서 사용해야함
+    Car2(color,"auto",5); // Car2가 아니라 this를 사용하기
+    }
+     */
+
+    Car2(){
+        this("white","auto",4);
+    }
+    Car2(String color){
+        this(color,"white",4);
+    }
+    Car2(String c,String g, int d){ // 지역변수랑 인스턴스 변수랑 이름이 다르면 this 생략가능
+        this.color = c;
+        this.gearType = g;
+        this.door = d;
+    }
+    /*
+    Car2(String color,String gearType, int door){ // 지역변수랑 인스턴스변수를 구분할려고 this 사용
+        this.color = color;
+        this.gearType = gearType;
+        this.door = door;
+
+    }*/
+}
